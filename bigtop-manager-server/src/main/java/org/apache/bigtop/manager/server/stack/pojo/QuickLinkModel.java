8eb5af59080d5af6cc32aa129453f97fae00dbb6
Zhiguo Wu <wuzhiguo@apache.org>
2024-07-16 16:57:15 +0800

    fix style

--- bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/stack/pojo/QuickLinkModel.java
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/stack/pojo/QuickLinkModel.java
@@ -18,10 +18,11 @@
  */
 package org.apache.bigtop.manager.server.stack.pojo;
 
+import lombok.Data;
+
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
-import lombok.Data;
 
 @Data
 @XmlAccessorType(XmlAccessType.FIELD)
