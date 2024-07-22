1282528af45e849e06ea0eb5c04b226f90c969f9
Zhiguo Wu <wuzhiguo@apache.org>
2024-07-22 11:10:47 +0800

    update shell executor log

--- bigtop-manager-common/src/main/java/org/apache/bigtop/manager/common/shell/ShellExecutor.java
+++ bigtop-manager-common/src/main/java/org/apache/bigtop/manager/common/shell/ShellExecutor.java
@@ -18,8 +18,9 @@
  */
 package org.apache.bigtop.manager.common.shell;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.bigtop.manager.common.thread.TaskLogThreadDecorator;
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.File;
