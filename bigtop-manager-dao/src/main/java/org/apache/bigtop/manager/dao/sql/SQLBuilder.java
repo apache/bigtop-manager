8dd681daf661d70dc03df890eefdf5113182c1fa
timyuer <524860213@qq.com>
2024-09-10 13:37:57 +0800

    BIGTOP-4217: Add findByCondition for MyBatis (#61)

--- bigtop-manager-dao/src/main/java/org/apache/bigtop/manager/dao/sql/SQLBuilder.java
+++ bigtop-manager-dao/src/main/java/org/apache/bigtop/manager/dao/sql/SQLBuilder.java
@@ -19,6 +19,10 @@
 
 package org.apache.bigtop.manager.dao.sql;
 
+import org.apache.bigtop.manager.common.utils.ClassUtils;
+import org.apache.bigtop.manager.dao.annotations.QueryCondition;
+import org.apache.bigtop.manager.dao.enums.DBType;
+
 import org.apache.ibatis.jdbc.SQL;
 
 import org.springframework.beans.BeanUtils;
@@ -29,7 +33,10 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.beans.PropertyDescriptor;
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.text.MessageFormat;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -195,11 +202,130 @@ public class SQLBuilder {
         return sql.toString();
     }
 
+    public static <Condition> String findByCondition(
+            TableMetaData tableMetaData, String databaseId, Condition condition) throws IllegalAccessException {
+        String tableName = tableMetaData.getTableName();
+        log.info("databaseId: {}", databaseId);
+        SQL sql = new SQL();
+        switch (DBType.toType(databaseId)) {
+            case MYSQL: {
+                sql = mysqlCondition(condition, tableName);
+                break;
+            }
+            default: {
+                log.error("Unsupported data source");
+            }
+        }
+
+        return sql.toString();
+    }
+
     private static String getEquals(String column, String property) {
         return column + " = " + getTokenParam(property);
     }
 
     private static String getTokenParam(String property) {
         return "#{" + property + "}";
+    }
+
+    private static <Condition> SQL mysqlCondition(Condition condition, String tableName) throws IllegalAccessException {
+
+        Class<?> loadClass;
+        try {
+            loadClass = condition.getClass();
+        } catch (Exception e) {
+            throw new RuntimeException("Get class Error!!!");
+        }
+
+        List<Field> fieldList = ClassUtils.getFields(loadClass);
+        /* Prepare SQL */
+        SQL sql = new SQL();
+        sql.SELECT("*");
+        sql.FROM(tableName);
+        for (Field field : fieldList) {
+            field.setAccessible(true);
+            String fieldName = field.getName();
+            log.debug("[requestField] {}, [requestValue] {}", fieldName, field.get(condition));
+            if (field.isAnnotationPresent(QueryCondition.class) && Objects.nonNull(field.get(condition))) {
+                QueryCondition annotation = field.getAnnotation(QueryCondition.class);
+
+                String queryKey = fieldName;
+                if (!annotation.queryKey().isEmpty()) {
+                    queryKey = annotation.queryKey();
+                }
+
+                log.info(
+                        "[queryKey] {}, [queryType] {}, [queryValue] {}",
+                        queryKey,
+                        annotation.queryType().toString(),
+                        field.get(condition));
+
+                Object value = field.get(condition);
+                if (value != null) {
+                    switch (annotation.queryType()) {
+                        case EQ:
+                            sql.WHERE(MessageFormat.format("{0} = ''{1}''", queryKey, value));
+                            break;
+                        case NOT_EQ:
+                            sql.WHERE(MessageFormat.format("{0} != ''{1}''", queryKey, value));
+                            break;
+                        case IN:
+                            sql.WHERE(MessageFormat.format(
+                                    "{0} IN (''{1}'')",
+                                    queryKey,
+                                    String.join("','", value.toString().split(annotation.multipleDelimiter()))));
+                            break;
+                        case NOT_IN:
+                            sql.WHERE(MessageFormat.format(
+                                    "{0} NOT IN (''{1}'')",
+                                    queryKey,
+                                    String.join("','", value.toString().split(annotation.multipleDelimiter()))));
+                            break;
+                        case GT:
+                            sql.WHERE(MessageFormat.format("{0} > ''{1}''", queryKey, value));
+                            break;
+                        case GTE:
+                            sql.WHERE(MessageFormat.format("{0} >= ''{1}''", queryKey, value));
+                            break;
+                        case LT:
+                            sql.WHERE(MessageFormat.format("{0} < ''{1}''", queryKey, value));
+                            break;
+                        case LTE:
+                            sql.WHERE(MessageFormat.format("{0} <= ''{1}''", queryKey, value));
+                            break;
+                        case BETWEEN:
+                            String[] valueArr = field.get(condition).toString().split(annotation.pairDelimiter());
+                            if (valueArr.length == 2) {
+                                sql.WHERE(MessageFormat.format(
+                                        "{0} BETWEEN ''{1}'' AND ''{2}''", queryKey, valueArr[0], valueArr[1]));
+                            }
+                            break;
+                        case PREFIX_LIKE:
+                            sql.WHERE(MessageFormat.format("{0} LIKE CONCAT(''{1}'', ''%'')", queryKey, value));
+                            break;
+                        case SUFFIX_LIKE:
+                            sql.WHERE(MessageFormat.format("{0} LIKE CONCAT(''%'', ''{1}'')", queryKey, value));
+                            break;
+                        case LIKE:
+                            sql.WHERE(MessageFormat.format("{0} LIKE CONCAT(''%'', ''{1}'', ''%'')", queryKey, value));
+                            break;
+                        case NOT_LIKE:
+                            sql.WHERE(MessageFormat.format(
+                                    "{0} NOT LIKE CONCAT(''%'', ''{1}'', ''%'')", queryKey, value));
+                            break;
+                        case NULL:
+                            sql.WHERE(queryKey + " IS NULL");
+                            break;
+                        case NOT_NULL:
+                            sql.WHERE(queryKey + " IS NOT NULL");
+                            break;
+                        default:
+                            log.warn("Unknown query type: {}", annotation.queryType());
+                    }
+                }
+            }
+        }
+
+        return sql;
     }
 }
