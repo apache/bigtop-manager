1d19ec1d949e785c396c27dcafc8cf974b39b43e
lhpqaq <657407891@qq.com>
2024-11-07 15:55:59 +0800

    add toolProvider

---
+++ bigtop-manager-server/src/main/java/org/apache/bigtop/manager/server/tools/ClusterInfoTools.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bigtop.manager.server.tools;
+
+import org.apache.bigtop.manager.dao.po.ClusterPO;
+import org.apache.bigtop.manager.dao.repository.ClusterDao;
+import org.apache.bigtop.manager.server.model.converter.ClusterConverter;
+
+import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.agent.tool.ToolSpecification;
+import dev.langchain4j.service.tool.ToolExecutor;
+
+import jakarta.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+public class ClusterInfoTools {
+    @Resource
+    private ClusterDao clusterDao;
+
+    @Tool("Get cluster list")
+    public Map<ToolSpecification, ToolExecutor> list() {
+        ToolSpecification toolSpecification = ToolSpecification.builder()
+                .name("list")
+                .description("Get cluster list")
+                .build();
+        ToolExecutor toolExecutor = (toolExecutionRequest, memoryId) -> {
+            List<ClusterPO> clusterPOList = clusterDao.findAll();
+            return ClusterConverter.INSTANCE.fromPO2VO(clusterPOList).toString();
+        };
+        return Map.of(toolSpecification, toolExecutor);
+    }
+}
