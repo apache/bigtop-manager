faa3f1974a5686823e05ffa3e6b01257cc6d3547
Zhiguo Wu <wuzhiguo@apache.org>
2024-12-09 17:16:24 +0800

    BIGTOP-4297: Install Tez with tarball (#120)

--- bigtop-manager-stack/bigtop-manager-stack-bigtop/src/main/java/org/apache/bigtop/manager/stack/bigtop/v3_3_0/tez/TezClientScript.java
+++ bigtop-manager-stack/bigtop-manager-stack-bigtop/src/main/java/org/apache/bigtop/manager/stack/bigtop/v3_3_0/tez/TezClientScript.java
@@ -18,21 +18,49 @@
  */
 package org.apache.bigtop.manager.stack.bigtop.v3_3_0.tez;
 
+import org.apache.bigtop.manager.common.message.entity.pojo.PackageInfo;
+import org.apache.bigtop.manager.common.message.entity.pojo.RepoInfo;
 import org.apache.bigtop.manager.common.shell.ShellResult;
 import org.apache.bigtop.manager.stack.core.spi.param.Params;
 import org.apache.bigtop.manager.stack.core.spi.script.AbstractClientScript;
 import org.apache.bigtop.manager.stack.core.spi.script.Script;
+import org.apache.bigtop.manager.stack.core.tarball.TarballDownloader;
 
 import com.google.auto.service.AutoService;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.List;
 
 @Slf4j
 @AutoService(Script.class)
 public class TezClientScript extends AbstractClientScript {
 
+    /**
+     * Tez tarball file doesn't need to be extracted, so here we override {@link #add(Params)} method
+     * and do nothing after download the tarball file.
+     *
+     * @param params the parameters required for installation
+     * @return ShellResult
+     */
+    @Override
+    public ShellResult add(Params params) {
+        RepoInfo repo = params.repo();
+        List<PackageInfo> packages = params.packages();
+
+        String repoUrl = repo.getBaseUrl();
+        String stackHome = params.stackHome();
+        for (PackageInfo packageInfo : packages) {
+            String remoteUrl = repoUrl + File.separator + packageInfo.getName();
+            TarballDownloader.download(remoteUrl, stackHome, packageInfo);
+        }
+
+        return ShellResult.success();
+    }
+
     @Override
     public ShellResult configure(Params params) {
-        return TezSetup.config(params);
+        return TezSetup.configure(params);
     }
 
     @Override
