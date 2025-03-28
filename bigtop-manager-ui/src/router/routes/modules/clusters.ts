9f6fc46fe554dcebd0db33f631be50072ad0db82
fuliqiang <1348994179@qq.com>
2025-03-28 17:15:44 +0800

    feat: Add service detail for infra

--- bigtop-manager-ui/src/router/routes/modules/clusters.ts
+++ bigtop-manager-ui/src/router/routes/modules/clusters.ts
@@ -74,7 +74,7 @@ const routes: RouteRecordRaw[] = [
           {
             name: 'ServiceDetail',
             path: `${RouteExceptions.DYNAMIC_ROUTE_MATCH}/service-detail/:service/:serviceId`,
-            component: () => import('@/pages/cluster-manage/cluster/service-manage/index.vue'),
+            component: () => import('@/pages/cluster-manage/cluster/service-management/index.vue'),
             meta: {
               hidden: true
             }
@@ -103,6 +103,15 @@ const routes: RouteRecordRaw[] = [
             name: 'CreateInfraService',
             path: 'create-infra-service/:id/:creationMode',
             component: () => import('@/components/create-service/create.vue'),
+            meta: {
+              hidden: true,
+              activeMenu: '/cluster-manage/infrastructures'
+            }
+          },
+          {
+            name: 'InfraServiceDetail',
+            path: 'create-infra-service/service-detail/:id/:service/:serviceId',
+            component: () => import('@/pages/cluster-manage/cluster/service-management/index.vue'),
             meta: {
               hidden: true,
               activeMenu: '/cluster-manage/infrastructures'
